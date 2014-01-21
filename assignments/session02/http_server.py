9fdfd0e560c1342f70591c494dc38f1bcdfd0a76
Fulvio Casali <fulviocasali@gmail.com>
2014-01-20 20:28:26 -0800

    adding a sample homework

--- assignments/session02/http_server.py
+++ assignments/session02/http_server.py
@@ -1,35 +1,105 @@
 import socket
 import sys
+import os
+
+# http server constants
+SERVER_IP = '127.0.0.1'
+SERVER_PORT = 10000
+BUFFER_SIZE = 1024
+SERVER_ROOT = 'webroot'
+CRLF = '\r\n'
 
 
-def response_ok():
+def response_ok(body="this is a pretty minimal response", mimetype="text/plain"):
     """returns a basic HTTP response"""
     resp = []
     resp.append("HTTP/1.1 200 OK")
-    resp.append("Content-Type: text/plain")
+    resp.append("Content-Type: %s" % mimetype)
     resp.append("")
-    resp.append("this is a pretty minimal response")
-    return "\r\n".join(resp)
+    resp.append(body)
+    return CRLF.join(resp)
 
 
+def get_mimetype(file_extension):
+    """returns the appropriate mime type for a given file extensions"""
+    mime_types = {
+        '.html': 'text/html',
+          '.py': 'text/x-python',
+         '.txt': 'text/plain',
+         '.jpg': 'image/jpeg',
+         '.png': 'image/png',
+          '.js': 'text/javascript',
+         '.css': 'text/css'
+    }
+    return mime_types.get(file_extension, 'Unknown')
+
+
+def resolve_uri(uri):
+    """
+    handles looking up resources on disk using the URI
+    Returns (body, mimetype) or raises ValueError if not found
+    
+    """
+
+    # map the pathname represented by the URI to a filesystem location    
+    path = os.path.join(SERVER_ROOT, uri.lstrip('/'))
+
+    if os.path.exists(path):
+        # the requested uri does in fact exist on the file system
+        
+        if os.path.isdir(path):
+            # uri is a directory; body should contain file listing
+            body = CRLF.join(os.listdir(path))
+            mimetype = get_mimetype('.txt')
+            
+        elif os.path.isfile(path):
+            # uri is a file; body should contain file contents
+            f = open(path, 'r')
+            body = f.read()
+            f.close() 
+
+            # and mimetype depends on file extension
+            filename = os.path.split(path)[1]
+            extension = os.path.splitext(filename)[1]
+            mimetype = get_mimetype(extension)
+        else:
+            # path exists but is neither directory nor file (Wut?)
+            body = None
+            mimetype = None
+        return (body, mimetype)
+
+    else:
+        raise ValueError("File not found on Server") 
+
+
+
+def response_not_found():
+    """returns a 404 code - File not found """
+    resp = []
+    resp.append("HTTP/1.1 404 Not Found")
+    resp.append("")
+    return CRLF.join(resp)
+
+    
 def response_method_not_allowed():
     """returns a 405 Method Not Allowed response"""
     resp = []
     resp.append("HTTP/1.1 405 Method Not Allowed")
     resp.append("")
-    return "\r\n".join(resp)
+    return CRLF.join(resp)
 
 
 def parse_request(request):
-    first_line = request.split("\r\n", 1)[0]
+    first_line = request.split(CRLF, 1)[0]
     method, uri, protocol = first_line.split()
     if method != "GET":
         raise NotImplementedError("We only accept GET")
     print >>sys.stderr, 'request is okay'
+    return uri
 
 
 def server():
-    address = ('127.0.0.1', 10000)
+    address = (SERVER_IP, SERVER_PORT)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     print >>sys.stderr, "making a server on %s:%s" % address
@@ -44,17 +114,21 @@ def server():
                 print >>sys.stderr, 'connection - %s:%s' % addr
                 request = ""
                 while True:
-                    data = conn.recv(1024)
+                    data = conn.recv(BUFFER_SIZE)
                     request += data
-                    if len(data) < 1024 or not data:
+                    if len(data) < BUFFER_SIZE or not data:
                         break
-
                 try:
-                    parse_request(request)
+                    # parse the request to get a URI
+                    uri = parse_request(request)
+                    # with the URI, find the appropriate resource on disk
+                    body, mimetype = resolve_uri(uri)
+                except ValueError:
+                        response = response_not_found()
                 except NotImplementedError:
                     response = response_method_not_allowed()
                 else:
-                    response = response_ok()
+                    response = response_ok(body, mimetype)
 
                 print >>sys.stderr, 'sending response'
                 conn.sendall(response)
